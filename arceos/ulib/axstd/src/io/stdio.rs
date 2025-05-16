64d1b48b04e29f53babb1702309ee2862bc99c71
dream <dreamveid060@gmail.com>
2025-05-16 13:24:59 +0800

    finish lab1 print_with_color

--- arceos/ulib/axstd/src/io/stdio.rs
+++ arceos/ulib/axstd/src/io/stdio.rs
@@ -25,7 +25,9 @@ impl Read for StdinRaw {
 
 impl Write for StdoutRaw {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        arceos_api::stdio::ax_console_write_bytes(buf)
+        arceos_api::stdio::ax_console_write_bytes(b"\x1b[32m")?;
+        arceos_api::stdio::ax_console_write_bytes(buf)?;
+        arceos_api::stdio::ax_console_write_bytes(b"\x1b[0m")
     }
     fn flush(&mut self) -> io::Result<()> {
         Ok(())
